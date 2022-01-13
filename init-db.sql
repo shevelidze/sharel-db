f443a3e716c31f1d06d69307075d3bcfd8ceba2e
Denys Shevel <denys.shevel16@gmail.com>
2022-01-13 21:14:14 +0200

    Change length of some fields

--- init-db.sql
+++ init-db.sql
@@ -1,9 +1,9 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    first_name VARCHAR(10) NOT NULL,
-    last_name VARCHAR(10) NOT NULL,
-    email VARCHAR(10) UNIQUE NOT NULL,
-    password_hash VARCHAR(10) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(70) UNIQUE NOT NULL,
+    password_hash VARCHAR(100) NOT NULL,
     password_set_timestamp TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
     registration_timestamp TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp
 );
@@ -14,11 +14,13 @@ CREATE TABLE posts (
     user_id INT REFERENCES users(id) ON DELETE CASCADE
 );
 CREATE TABLE likes (
-    post_id INT REFERENCES posts(id),
+    id SERIAL PRIMARY KEY,
+    post_id INT REFERENCES posts(id) ON DELETE CASCADE,
     user_id INT REFERENCES users(id)
 );
 CREATE TABLE views (
-    post_id INT REFERENCES posts(id),
+    id SERIAL PRIMARY KEY,
+    post_id INT REFERENCES posts(id) ON DELETE CASCADE,
     user_id INT REFERENCES users(id)
 );
 CREATE TABLE subscriptions (
