838fc5b9c10e6834d33a707959dc330e7252ed27
Denys Shevel <denys.shevel16@gmail.com>
2022-07-06 09:01:33 -0400

    Modify intitial file

--- init-db.sql
+++ init-db.sql
@@ -1,3 +1,10 @@
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS sessions CASCADE;
+DROP TABLE IF EXISTS posts CASCADE;
+DROP TABLE IF EXISTS likes CASCADE;
+DROP TABLE IF EXISTS views CASCADE;
+DROP TABLE IF EXISTS subscriptions CASCADE;
+
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     first_name TEXT NOT NULL,
@@ -6,29 +13,36 @@ CREATE TABLE users (
     password_hash VARCHAR(64) NOT NULL,
     registration_timestamp TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp
 );
-CREATE TABLE refreshSessions (
+
+CREATE TABLE sessions (
+    id SERIAL PRIMARY KEY,
+    refresh_token_id INT NOT NULL DEFAULT 1,
     user_id INT REFERENCES users(id) ON DELETE CASCADE,
-    refresh_token_hash TEXT PRIMARY KEY,
-    user_agent TEXT NOT NULL,
-    ip_address TEXT NOT NULL,
-    authentification_timestamp TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp
+    user_agent TEXT,
+    ip_address TEXT,
+    authentification_timestamp TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp NOT NULL,
+    last_used_timestamp TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp NOT NULL
 );
+
 CREATE TABLE posts (
     id SERIAL PRIMARY KEY,
     content TEXT NOT NULL,
     creation_timestamp TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
     user_id INT REFERENCES users(id) ON DELETE CASCADE
 );
+
 CREATE TABLE likes (
     id SERIAL PRIMARY KEY,
     post_id INT REFERENCES posts(id) ON DELETE CASCADE,
     user_id INT REFERENCES users(id)
 );
+
 CREATE TABLE views (
     id SERIAL PRIMARY KEY,
     post_id INT REFERENCES posts(id) ON DELETE CASCADE,
     user_id INT REFERENCES users(id)
 );
+
 CREATE TABLE subscriptions (
     subscriber_user_id INT REFERENCES users(id) ON DELETE CASCADE,
     user_id INT REFERENCES users(id),
