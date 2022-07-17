58a5a29c53dd01250de147cd1f7e8df17e7f8af4
Denys Shevel <denys.shevel16@gmail.com>
2022-07-17 06:22:59 -0400

    Make all not nullable

--- init-db.sql
+++ init-db.sql
@@ -11,15 +11,15 @@ CREATE TABLE users (
     last_name TEXT NOT NULL,
     email TEXT UNIQUE NOT NULL,
     password_hash VARCHAR(64) NOT NULL,
-    registration_timestamp TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp
+    registration_timestamp TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp NOT NULL
 );
 
 CREATE TABLE sessions (
     id SERIAL PRIMARY KEY,
     refresh_token_id INT NOT NULL DEFAULT 1,
-    user_id INT REFERENCES users(id) ON DELETE CASCADE,
-    user_agent TEXT,
-    ip_address TEXT,
+    user_id INT REFERENCES users(id) ON DELETE CASCADE NOT NULL,
+    user_agent TEXT NOT NULL,
+    ip_address TEXT NOT NULL,
     authentification_timestamp TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp NOT NULL,
     last_used_timestamp TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp NOT NULL
 );
@@ -27,24 +27,24 @@ CREATE TABLE sessions (
 CREATE TABLE posts (
     id SERIAL PRIMARY KEY,
     content TEXT NOT NULL,
-    creation_timestamp TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
-    user_id INT REFERENCES users(id) ON DELETE CASCADE
+    creation_timestamp TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp NOT NULL,
+    user_id INT REFERENCES users(id) ON DELETE CASCADE NOT NULL
 );
 
 CREATE TABLE likes (
     id SERIAL PRIMARY KEY,
-    post_id INT REFERENCES posts(id) ON DELETE CASCADE,
-    user_id INT REFERENCES users(id)
+    post_id INT REFERENCES posts(id) ON DELETE CASCADE NOT NULL,
+    user_id INT REFERENCES users(id) NOT NULL
 );
 
 CREATE TABLE views (
     id SERIAL PRIMARY KEY,
-    post_id INT REFERENCES posts(id) ON DELETE CASCADE,
-    user_id INT REFERENCES users(id)
+    post_id INT REFERENCES posts(id) ON DELETE CASCADE NOT NULL,
+    user_id INT REFERENCES users(id) NOT NULL
 );
 
 CREATE TABLE subscriptions (
-    subscriber_user_id INT REFERENCES users(id) ON DELETE CASCADE,
-    user_id INT REFERENCES users(id),
+    subscriber_user_id INT REFERENCES users(id) ON DELETE CASCADE NOT NULL,
+    user_id INT REFERENCES users(id) NOT NULL,
     PRIMARY KEY (subscriber_user_id, user_id)
 );
